a7a7e11302096932ad7e0c23eed76388bda66b81
Ilse Centeno <Ilse Centeno>
2017-04-11 12:12:42 -0500

    version 4

---
+++ Js/main.js
@@ -0,0 +1,16 @@
+
+var soccer= document.getElementById("soccer");
+soccer.addEventListener("click", mostrarSoccer);
+
+function mostrarSoccer(){
+	var canchaSoccer= document.getElementById("canchaSoccer");
+	canchaSoccer.style.display="block";
+}
+
+var basketball= document.getElementById("basketball");
+ 	basketball.addEventListener("click", mostrarBasketball);
+
+ function mostrarBasketball(){
+ 	var canchaBasketball= document.getElementById("canchaBasketball");
+ 	canchaBasketball.style.display="block";
+ }	
